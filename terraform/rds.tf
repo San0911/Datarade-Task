16ef53761383ab0e4b41f3d55b8972f19b95b76e
San0911 <161283747+San0911@users.noreply.github.com>
2024-12-20 19:11:57 +0100

    Update rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -2,7 +2,7 @@ resource "aws_db_instance" "postgres_db" {
   allocated_storage    = 5
   engine               = "postgres"
   engine_version       = "13"
-  instance_class       = "db.t2.medium"
+  instance_class       = "db.t3.medium" # db.t2.medium not supported for V13
   db_name              = var.db_name
   username             = var.db_username
   password             = var.db_password
