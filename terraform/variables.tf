fd7c965998ab226953798ecbc3d2d4379913ffcc
San0911 <161283747+San0911@users.noreply.github.com>
2024-12-20 18:19:39 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,7 +2,9 @@ variable "ubuntu_ami_id" {
   default = "ami-0d5eff06f840b45e9" # Ubuntu 20.04 in us-east-1
 }
 
-variable "key_name" {}
+variable "key_name" {
+  default = "mykeypair"
+}
 variable "private_key_path" {}
 
 variable "db_name" {}
