ce9fea5f7ec801ff75798a7c6781ccf634f76d28
San0911 <161283747+San0911@users.noreply.github.com>
2024-12-20 18:23:28 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,21 +4,21 @@ provider "aws" {
 
 # Include all resource files
 module "network" {
-  source = "./terraform/network.tf"
+  source = "network.tf"
 }
 
 module "ec2" {
-  source = "./terraform/ec2.tf"
+  source = "ec2.tf"
 }
 
 module "rds" {
-  source = "./terraform/rds.tf"
+  source = "rds.tf"
 }
 
 module "security_groups" {
-  source = "./terraform/security_groups.tf"
+  source = "security_groups.tf"
 }
 
 module "backend" {
-  source = "./terraform/backend.tf"
+  source = "backend.tf"
 }
