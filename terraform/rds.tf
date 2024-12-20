dfecb54b9f912d121c5779129b63dacc08f1225e
San0911 <161283747+San0911@users.noreply.github.com>
2024-12-20 16:39:02 +0100

    Create rds.tf

---
+++ terraform/rds.tf
@@ -0,0 +1,20 @@
+resource "aws_db_instance" "postgres_db" {
+  allocated_storage    = 5
+  engine               = "postgres"
+  engine_version       = "13"
+  instance_class       = "db.t2.micro"
+  name                 = var.db_name
+  username             = var.db_username
+  password             = var.db_password
+  publicly_accessible  = true
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  skip_final_snapshot  = true
+
+  tags = {
+    Name = "PostgresDB"
+  }
+}
+
+output "db_endpoint" {
+  value = aws_db_instance.postgres_db.endpoint
+}
